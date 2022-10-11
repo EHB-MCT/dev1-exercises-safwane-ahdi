79a7c7116ed59c97f1379d628ec4e7205fb6edb3
Safwane Ahdi <safwane.ahdi@student.ehb.be>
2022-10-11 17:34:46 +0200

    red color to stroke added

--- exercises/1-functions/1-3-rectangles.js
+++ exercises/1-functions/1-3-rectangles.js
@@ -10,6 +10,7 @@ drawLine();
 function drawLine() {
 
     context.fillStyle = "white";
+    context.strokeStyle = "red";
     context.lineWidth = 1;
     context.beginPath();
     context.rect(50, 50, 100, 100);
@@ -22,8 +23,9 @@ function drawLine() {
     context.rect(150, 150, 100, 100);
     context.fill();
     context.stroke();
-
+    
     context.fillStyle = "black";
+    context.strokeStyle = "black";
     context.lineWidth = 1;
     context.beginPath();
     context.rect(100, 100, 100, 100);
@@ -31,6 +33,7 @@ function drawLine() {
     context.stroke();
 
     context.fillStyle = "white";
+    context.strokeStyle = "red";
     context.lineWidth = 1;
     context.beginPath();
     context.rect(200, 70, 30, 30);
@@ -38,6 +41,7 @@ function drawLine() {
     context.stroke();
 
     context.fillStyle = "black";
+    context.strokeStyle = "black";
     context.lineWidth = 1;
     context.beginPath();
     context.rect(232, 50, 20, 20);
@@ -45,6 +49,7 @@ function drawLine() {
     context.stroke();
 
     context.fillStyle = "white";
+    context.strokeStyle = "red";
     context.lineWidth = 1;
     context.beginPath();
     context.rect(70, 200, 30, 30);
@@ -52,10 +57,13 @@ function drawLine() {
     context.stroke();
 
     context.fillStyle = "black";
+    context.strokeStyle = "black";
     context.lineWidth = 1;
     context.beginPath();
     context.rect(50, 230, 20, 20);
     context.fill();
     context.stroke();
+
+
 
 }
