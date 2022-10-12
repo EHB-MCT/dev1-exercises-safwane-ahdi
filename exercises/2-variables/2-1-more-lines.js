99792f74934d56162479d0fead8b30624679091e
safwane-ahdi <safwane.ahdi@student.ehb.be>
2022-10-12 12:21:24 +0200

    made 2-2

---
+++ exercises/2-variables/2-1-more-lines.js
@@ -0,0 +1,27 @@
+"use strict";
+
+let canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext('2d');
+
+let width = context.canvas.width;
+let height = context.canvas.height;
+
+let margin = 50;
+let spacing = 50;
+
+drawLines();
+
+function drawLines(){
+
+    context.beginPath();
+    context.moveTo(margin, spacing);
+    context.lineTo(width - margin, spacing);
+    context.lineTo(margin, spacing * 2);
+    context.lineTo(width - margin, spacing * 2);
+    context.lineTo(margin, spacing * 3);
+    context.lineTo(width - margin, spacing * 4)
+    context.stroke();
+    
+}
---
+++ exercises/2-variables/2-2-random.js
@@ -0,0 +1,57 @@
+"use strict";
+let canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext('2d');
+
+let width = context.canvas.width;
+let height = context.canvas.height;
+
+/*
+let size = 400;
+let randomX = Math.floor(Math.random() * (width - size));
+let randomY = Math.floor(Math.random() * (height - size));
+
+let r = Math.floor(Math.random() * 255);
+let g = Math.floor(Math.random() * 255);
+let b = Math.floor(Math.random() * 255);
+console.log(r, g, b);
+let color ="rgb(" + r + "," + g + "," + b + ")";
+
+context.fillStyle = color;
+context.fillRect(randomX, randomY, size, size);
+*/
+
+let size = 500;
+let positionX = 100;
+let positionY = 100;
+
+function randomColor () {
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    console.log(r, g, b);
+    let color ="rgb(" + r + "," + g + "," + b + ")";
+    return color;
+}
+
+function drawRect(){
+    context.fillStyle = randomColor();
+    context.fillRect(positionX, positionY, size, size);
+    changeSizeRect();
+}
+
+function changeSizeRect(){
+    positionX = positionX + 25;
+    positionY = positionY + 25;
+    size = size - 50;
+}
+
+drawRect();
+drawRect();
+drawRect();
+drawRect();
+drawRect();
+drawRect();
+drawRect();
+drawRect();
